9f04c394ad08433b5688884cee38050365dfb1ed
Rami Ismael <rami.m.ismael@gmail.com>
2026-02-16 14:06:31 -0600

    Implement authentication logic in TopNavBar and add useAuthStore hook

--- asfm-fe/src/components/NonMemberSignInNavBar.jsx
+++ asfm-fe/src/components/NonMemberSignInNavBar.jsx
@@ -1,6 +1,8 @@
 import { Button } from "./ui/button";
+import { useAuthStore } from "../hooks/useAuthStore";
 
-function TopNavBar({ status, username }) {
+function TopNavBar() {
+  const { isAuthenticated, login, logout } = useAuthStore();
   return (
     <nav className="bg-secondary p-4 flex justify-between">
       <div className="flex flex-grid items-center gap-4">
@@ -8,18 +10,18 @@ function TopNavBar({ status, username }) {
          <div  className="rounded-md-10  text-secondary-foreground border-black-400 px-4 py-2">Logo</div>
         <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Home</Button>
         {/* is status it not guess add animal div */}
-        {status !== "guest" && <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Animal</Button>}
+        {isAuthenticated && <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Animal</Button>}
       </div>
       <div className="flex items-center gap-4">
-        {status === "guest" ? (
+        {!isAuthenticated ? (
           <>
-            <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Sign In</Button>
+            <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary" onClick={login}>Sign In</Button>
             <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Sign Up</Button>
           </>
         ) : (
           <div className="flex items-center gap-4">
             <Button variant="outline" className="text-secondary-foreground">Profile</Button>
-            <Button variant="outline" className="text-secondary-foreground">Logout</Button>
+            <Button variant="outline" className="text-secondary-foreground" onClick={logout}>Logout</Button>
           </div>
         )}
       </div>
