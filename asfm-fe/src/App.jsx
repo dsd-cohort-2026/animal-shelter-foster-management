f33db7a3984f964ccf2c446e74557abd4e196e5d
Rami Ismael <rami.m.ismael@gmail.com>
2026-02-17 16:21:12 -0600

    Remove the uncessary zustland example in App.jsx

--- asfm-fe/src/App.jsx
+++ asfm-fe/src/App.jsx
@@ -15,16 +15,6 @@ function App() {
       <Button className="bg-secondary text-secondary-foreground font-montserrat text-h2 px-xl py-xl">
         Testing ShadeCn Config
       </Button>
-        <Button variant="outline" onClick={() => addPet({ name: "Dog", type: "dog" })}>Add Dog</Button>
-        <Button variant="outline" onClick={() => addPet({ name: "Cat", type: "cat" })}>Add Cat</Button>
-        <div>
-        {pets.map((pet) => (
-            <div key={pet.id} className="p-4 border rounded">
-              <h3 className="text-lg font-semibold">{pet.name}</h3>
-              <p className="text-sm text-gray-500">{pet.type}</p>
-            </div>
-          ))}
-        </div>
       </div>
     </div>
   )
