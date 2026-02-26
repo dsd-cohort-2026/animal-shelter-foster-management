c6eead4e826379c2de17e41702cfe841d4152cb4
Leon Zhao <lzhao92@gmail.com>
2026-02-26 00:33:32 -0500

    added SignInForm and basical validation

---
+++ asfm-fe/src/components/SignInForm.jsx
@@ -0,0 +1,110 @@
+import { Button } from '@/components/ui/button';
+import { Field, FieldError, FieldGroup, FieldLabel, FieldSet } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
+import { useForm } from '@tanstack/react-form';
+import * as z from 'zod';
+
+const formSchema = z.object({
+  email: z.email({
+    message: 'Invalid email address',
+  }),
+  password: z.string().min(1, { message: 'Password is required' }),
+});
+
+function SignInForm() {
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
+      form.reset();
+    },
+  });
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Card className="w-100 m-auto">
+        <CardHeader>
+          {/* <h2 className="text-center text-4xl">Sign In</h2> */}
+          <CardTitle className="text-3xl text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Enter your email below to sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            id="sign-in-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              form.handleSubmit();
+            }}
+          >
+            <FieldSet className="gap-7">
+              <FieldGroup>
+                <form.Field
+                  name="email"
+                  children={(field) => {
+                    const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                        <Input
+                          id={field.name}
+                          name={field.name}
+                          type="email"
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          placeholder="Email address"
+                          aria-invalid={isInvalid}
+                        />
+                        {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                      </Field>
+                    );
+                  }}
+                />
+                <form.Field
+                  name="password"
+                  children={(field) => {
+                    const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                        <Input
+                          id={field.name}
+                          name={field.name}
+                          type="password"
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          placeholder="Password"
+                          aria-invalid={isInvalid}
+                        />
+                        {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                      </Field>
+                    );
+                  }}
+                />
+              </FieldGroup>
+            </FieldSet>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <FieldGroup>
+            <Button type="submit" form="sign-in-form" className="w-[70%] m-auto">
+              Sign In
+            </Button>
+          </FieldGroup>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default SignInForm;
