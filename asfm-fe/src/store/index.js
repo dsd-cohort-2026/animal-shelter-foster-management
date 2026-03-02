a5389204cfa7f0b8a712fffd49cca81b924a25ca
Leon Zhao <lzhao92@gmail.com>
2026-03-02 12:58:11 -0500

    added devtools to zustand store

--- asfm-fe/src/store/index.js
+++ asfm-fe/src/store/index.js
@@ -2,10 +2,11 @@ import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 import { userAnimalsSlice } from './slices/userAnimalsSlice';
 import { createAuthSlice } from './slices/createAuthSlice';
+import { devtools } from 'zustand/middleware';
 
-export const useBoundStore = create(
+export const useBoundStore = create(devtools(
   immer((...a) => ({
     ...userAnimalsSlice(...a),
     ...createAuthSlice(...a),
   })),
-);
+));
