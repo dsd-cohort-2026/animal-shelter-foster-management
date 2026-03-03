5daed41def08584f8995d44db495fd7f64a007d8
Ryan Griego <ryangriego@gmail.com>
2026-03-03 10:26:52 -0800

    ANIMALS-108- Added ability to filter users in get endpoints and take into account staff/foster. Also added a test file.

--- server/features/animals/animals.routes.js
+++ server/features/animals/animals.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const animalController = require('./animals.controller');
 const staffAuthCheck = require('../../middleware/auth-staff');
+const authUserCheck = require('../../middleware/auth-user');
 const validate = require('../../middleware/validator');
 const createAnimalSchema = require('../../validators/animals/createAnimal.validator');
+const getAllAnimalsSchema = require('../../validators/animals/animals.validator');
 
-router.get('/', animalController.getAllAnimals);
+router.get('/', authUserCheck, validate(getAllAnimalsSchema), animalController.getAllAnimals);
 router.get('/:id', animalController.getAnimalById);
 router.post('/create', staffAuthCheck, validate(createAnimalSchema), animalController.createAnimal);
 
